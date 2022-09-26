90e9d8d7e66f5ce3db2dae118705bfb587f1e5f8
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-09-26 20:30:24 +0500

    Code Updated and corrected

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
 const cur1 = document.querySelector(".cur-1");
 const cur2 = document.querySelector(".cur-2");
-const cur1input = document.querySelector("cur-1-input");
-const cur2input = document.querySelector("cur-2-input");
-const baserate = document.querySelector(".base");
-const switchcur = document.querySelector(".switch-cur");
+const cur1Input = document.querySelector(".cur-1-input");
+const cur2Input = document.querySelector(".cur-2-input");
+
+const baseRate = document.querySelector(".base");
+const switchCur = document.querySelector(".switch-cur");
 
 const countries = [
   {
     name: "AED",
-    FLAGURL: "https://www.worldometers.info/img/flags/ae-flag.gif",
+    flagURL: "https://www.worldometers.info/img/flags/ae-flag.gif",
   },
   {
     name: "EUR",
-    FLAGURL: "https://www.worldometers.info/img/flags/au-flag.gif",
+    flagURL: "https://www.worldometers.info/img/flags/au-flag.gif",
   },
   {
     name: "GBP",
-    FLAGURL: "https://www.worldometers.info/img/flags/uk-flag.gif",
+    flagURL: "https://www.worldometers.info/img/flags/uk-flag.gif",
   },
   {
     name: "USD",
-    FLAGURL: "https://www.worldometers.info/img/flags/us-flag.gif",
+    flagURL: "https://www.worldometers.info/img/flags/us-flag.gif",
   },
 ];
 
 const apiURL = "https://v6.exchangerate-api.com/v6/";
 const key = "093352694b431c8342cff984";
 
-async function getexchangerate() {
-  const cur1val = cur1.value;
-  const cur2val = cur2.value;
-  const response = await fetch(`${apiURL}${key}/latest/${cur1val}`);
-  const data = await response.json();
-  console.log(data.conversion_rates);
-  const rate = data.conversion_rates[cur2val];
-  baserate.textContent = `1 ${cur1val} = ${rate.toFixed(2)} ${cur2val}`;
-  cur2input.value = (cur1input.value * rate).toFixed(2);
-}
-getexchangerate();
+// Get Exchange Rate
+async function getExchangeRate() {
+  const cur1Value = cur1.value;
+  const cur2Value = cur2.value;
 
+  const response = await fetch(`${apiURL}${key}/latest/${cur1Value}`);
+  const data = await response.json();
+  console.log(data);
+
+  const rate = data.conversion_rates[cur2Value];
+
+  baseRate.textContent = `1 ${cur1Value} = ${rate.toFixed(2)} ${cur2Value}`;
+
+  cur2Input.value = (cur1Input.value * rate).toFixed(2);
+}
+
+// Add Event Listeners
 cur1.addEventListener("change", () => {
-  getexchangerate();
+  getExchangeRate();
 });
 cur2.addEventListener("change", () => {
-  getexchangerate();
+  getExchangeRate();
 });
-cur1input.addEventListener("input", getexchangerate);
-cur2input.addEventListener("input", getexchangerate);
+cur1Input.addEventListener("input", getExchangeRate);
+cur2Input.addEventListener("input", getExchangeRate);
 
 function getflag() {
   countries.forEach((country) => {
